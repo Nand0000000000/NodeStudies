a987c8f65b4ab6977a84e824a7ea286551637ce1
Nand0000000000 <flgm0504@gmail.com>
2025-04-22 21:39:16 -0300

    server update

--- fundamentos-node/src/server.js
+++ fundamentos-node/src/server.js
@@ -2,8 +2,22 @@ import http from 'node:http';
 
 const users =[];
 
-const server = http.createServer ((req, res)=>{
+const server = http.createServer (async(req, res)=>{
     const {method, url} = req;
+
+    const buffers = [];
+
+    for await (const chunk of req){
+        buffers.push(chunk)
+    }
+
+    try{
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    }catch{
+        req.body = null
+    }
+
+    console.log(req.body)
 
     if (method === 'POST' && url === '/users'){
         users.push({
